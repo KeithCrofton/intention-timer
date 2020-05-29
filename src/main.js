8a3db448bfaa738aface879459147fa731b6fb24
Keith Crofton <keithcrofton@gmail.com>
2020-05-28 21:17:40 -0600

    Finished de-colorizing the buttons and started the start button

--- src/main.js
+++ src/main.js
@@ -1,25 +1,41 @@
 var mainSection = document.querySelector('.main-section');
+var activities = [];
 
 mainSection.addEventListener('click', handleClick);
 
 function handleClick(event) {
-  var parentClass = event.target.parentElement.className
-  var childClass = event.target.className
+  var parentClass = event.target.parentElement.className;
+  var childClass = event.target.className;
 
   if (parentClass == "category-buttons" || childClass == "button-img") {
-    activateButton(event)
-  }
+    determineButtonStyle(event)
+  } else if (childClass == "start-button") {
+    startActivity();
+    }
 }
+
 //Category Button Functions
 function identifyButton(event) {
   return event.target.id;
 }
 
-function activateButton(event) {
+function deactivateButton() {
+  var active;
+  var notActive;
+  for ( i = 0; i < 3; i++ ){
+      document.querySelectorAll("button")[i].style.color = "#FFF";
+      document.querySelectorAll("button")[i].style.borderColor = "#FFF";
+
+      active = document.querySelectorAll("button")[i].innerHTML;
+      notActive = active.replace("-active", "");
+      document.querySelectorAll("button")[i].innerHTML = notActive;
+  }
+}
+
+function determineButtonStyle(event) {
   var id = identifyButton(event);
   var color;
   var src;
-  debugger
   if (id == "studyButton") {
     color = "#B3FD78";
     src = "./assets/study-active.svg";
@@ -34,14 +50,26 @@ function activateButton(event) {
 }
 
 function colorButton(id, color, src) {
+  deactivateButton();
   document.getElementById(id).style.color = color;
   document.getElementById(id).style.borderColor = color;
   document.querySelectorAll(`#${id}`)[1].src = src;
 }
+//start button
 
-function addActive(event) {
+startActivity() {
 
 }
+
+saveActivity() {
+  activities
+}
+// startActivity(){
+// take the values from the inputs and turn them into
+// arguements that get pushed into our activity constuctors
+// step 1 remove the activity form
+// step 2 place the clock
+// }
 
 //form functionality
 //target the parent element with a query selector
