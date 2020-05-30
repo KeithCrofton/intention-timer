86efdecfeb24782f4a10f32337845a2d8f7cdde7
Greyson Elkins <greysonelkins@gmail.com>
2020-05-29 22:02:48 -0600

    Make the clock operate in the 4th dimension

--- src/main.js
+++ src/main.js
@@ -12,7 +12,9 @@ function handleClick(event) {
     determineButtonStyle(event)
   } else if (childClass == "start-button") {
     startActivity();
-    }
+  } else if (childClass == "start-clock") {
+    var interval = setInterval(countDown, 1000);
+  }
 }
 
 //Category Button Functions
@@ -49,6 +51,9 @@ function determineButtonStyle(event) {
     color = "#FD8078";
     src = "./assets/exercise-active.svg";
   }
+
+  document.querySelector('.start-clock').style.borderColor = color;
+
   colorButton(id, color, src);
 }
 
@@ -63,6 +68,8 @@ function colorButton(id, color, src) {
 function startActivity() {
   saveActivity();
   hideElement(document.querySelector(".activity-maker"));
+  showElement(document.querySelector(".clock-view"));
+  setUpClock();
 }
 
 function saveActivity() {
@@ -77,16 +84,33 @@ function saveActivity() {
 function hideElement(element) {
   element.classList.add("hidden")
 }
-// startActivity(){
-// take the values from the inputs and turn them into
-// arguements that get pushed into our activity constuctors
-// step 1 remove the activity form
-// step 2 place the clock
-// }
 
-//form functionality
-//target the parent element with a query selector
-//add event listener
-//create an event handler
-//create a funtion that changes the border color while reseting the other border colors/ one that
-//changes the border color should also chasnge the image.
+function showElement(element) {
+  element.classList.remove("hidden");
+}
+
+function setUpClock() {
+ document.querySelector(".activity-description").innerText = activities[0].description;
+ document.querySelector(".remaining-time").innerText = `${activities[0].minutes}:${activities[0].seconds}`;
+}
+
+function countDown() {
+  var stringTime = document.querySelector(".remaining-time").innerText;
+  var countMinuteMS = Number(stringTime.slice(0, -3)) * 60000;
+  var countSecondMS = stringTime.slice(-2, stringTime.length) * 1000;
+  var timeMS = countMinuteMS + countSecondMS;
+
+  timeMS -= 1000;
+
+  var reminutes = (timeMS - (timeMS % 60000)) / 60000;
+  var reseconds = timeMS % 60000 / 1000;
+
+  if(timeMS <= 0) {
+    document.querySelector(".remaining-time").innerText = `0:00`;
+  } else if (reseconds < 10) {
+    reseconds = `0${reseconds}`;
+    document.querySelector(".remaining-time").innerText = `${reminutes}:${reseconds}`;
+  } else {
+    document.querySelector(".remaining-time").innerText = `${reminutes}:${reseconds}`;
+  }
+}
